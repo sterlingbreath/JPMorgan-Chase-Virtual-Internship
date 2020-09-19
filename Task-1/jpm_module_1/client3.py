94a05f543242e27648b3a602c5ad4cc0c33241fd
kritika-srivastava <kritika.s1408@gmail.com>
2020-09-19 22:07:03 +0530

    Initial Commit

---
+++ Task-1/jpm_module_1/client3.py
@@ -0,0 +1,49 @@
+#This is where you would manually type the existing code in the client3.py, which is inside the jpm_module_1 folder (disregard copying the useless comments) so that you can avoid the dreaded tab editing issues in REPL particularly for the Python environment only
+
+#After manually copying the useful code in client3.py, you can make your changes here
+
+#Before you test your modified code is working, make sure you have done the initial steps needed to be done before making changes in the instruction slides
+
+#Then, to test, first, delete the client3.py file inside jpm_module_1. Then rename this file to just client3.py. Finally move this file inside the jpm_module_1 folder  
+
+# Delete these comments when you're done...
+# Comments are anything that's preceded with '#'
+import urllib.request
+import time
+import json
+import random
+
+# Server API URLs
+QUERY = "http://localhost:8080/query?id={}"
+
+# 500 server request
+N = 500
+
+def getDataPoint(quote):
+	""" Produce all of the needed values to generate a datapoint """
+	""" ------------- Update this function ------------- """
+	stock = quote['stock']
+	bid_price = float(quote['top_bid']['price'])
+	ask_price = float(quote['top_ask']['price'])
+	price = bid_price
+	return stock, bid_price, ask_price, price
+
+def getRatio(price_a, price_b):
+	""" Get ratio of price_a and price_b """
+	""" ------------- Update this function ------------- """
+	""" Also create some unit tests for this function in client_test.py """
+	return 1
+
+# Main
+if __name__ == "__main__":
+
+	# Query the price once every N seconds.
+	for _ in iter(range(N)):
+		quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+
+		""" ----------- Update to get the ratio --------------- """
+		for quote in quotes:
+			stock, bid_price, ask_price, price = getDataPoint(quote)
+			print ("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+		print ("Ratio %s" % getRatio(price, price))
